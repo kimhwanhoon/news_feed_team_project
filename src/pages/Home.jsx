aba62e8323b37c62885d7e952c3c1ec4c7f949ab
TerryEHLee <terryehlee@gmail.com>
2023-06-26 20:30:39 +0900

    create

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import Input from 'redux/components/Input';
+import Searchbar from 'redux/components/Searchbar';
+import FeedList from '../redux/components/FeedList';
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <h2>메인 페이지</h2>
+      <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div>FeedIT</div>
+        <div style={{ flex: '1', textAlign: 'center' }}>
+          <Searchbar />
+        </div>
+        <button>Login</button>
+      </nav>
+      <form style={{ flex: '1', textAlign: 'center' }}>
+        <button style={{ marginTop: '20px' }}>New feed</button>
+      </form>
+      <Input />
+      <div>
+        <FeedList />
+      </div>
     </div>
   );
 }
---
+++ src/redux/components/Input.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import uuid from 'react-uuid';
+import { addFeed } from '../modules/feeds';
+
+function Input() {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmitButtonClick = (e) => {
+    e.preventDefault();
+
+    const newFeed = {
+      id: uuid(),
+      title,
+      body
+    };
+
+    dispatch(addFeed(newFeed));
+    setTitle('');
+    setBody('');
+  };
+
+  const handleTitleInputChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleBodyInputChange = (e) => {
+    setBody(e.target.value);
+  };
+
+  return (
+    <StyledInputBox>
+      <form onSubmit={handleSubmitButtonClick}>
+        <label>✒️ Writing a new feed</label>
+        <input type="text" value={title} onChange={handleTitleInputChange} placeholder="Title" />
+        <input value={body} onChange={handleBodyInputChange} placeholder="Body" />
+        <button type="submit">Submit</button>
+      </form>
+    </StyledInputBox>
+  );
+}
+
+const StyledInputBox = styled.div`
+  background-color: #616464;
+  padding: 20px;
+`;
+
+export default Input;
